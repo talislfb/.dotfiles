85dffc3bc864619a569dbaaafa92a904dc991832
Talis Lincoln <talis.lfb@gmail.com>
2021-12-02 13:10:06 -0500

    added powerline fonts installation

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo pacman -S stow
+sudo pacman -S stow curl vim
+
+#install powerline fonts
+git clone git@github.com:powerline/fonts.git
+pushd fonts
+./install.sh
+popd
+rm -r fonts
